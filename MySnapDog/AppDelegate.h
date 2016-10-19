3f6451250366797d44dde2019c340c77e7d7f042
ITE <ite@ites-macbook-pro-2.local>
2016-10-19 13:34:07 +0800

    Initial Commit

---
+++ MySnapDog/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MySnapDog
+//
+//  Created by ITE on 19/10/16.
+//  Copyright © 2016 ITE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MySnapDog/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MySnapDog
+//
+//  Created by ITE on 19/10/16.
+//  Copyright © 2016 ITE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
